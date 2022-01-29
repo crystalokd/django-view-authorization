624bceb653dafba14e4b5e68fba92bf4d8ade3d8
crystal <crstalokd@gmail.com>
2022-01-29 11:46:29 +0100

    request

--- core/views.py
+++ core/views.py
@@ -16,3 +16,31 @@ def view_blog(request, blog_id):
     }
 
     return render(request, "view_blog.html", data)
+
+
+def see_request(request):
+    text = f"""
+        Some attributes of the HttpRequest object:
+
+        scheme: {request.scheme}
+        path:   {request.path}
+        method: {request.method}
+        GET:    {request.GET}
+        user:   {request.user}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
+
+
+def user_info(request):
+    text = f"""
+        Selected HttpRequest.user attributes:
+
+        username:     {request.user.username}
+        is_anonymous: {request.user.is_anonymous}
+        is_staff:     {request.user.is_staff}
+        is_superuser: {request.user.is_superuser}
+        is_active:    {request.user.is_active}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
